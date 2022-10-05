91b6c7fcabca96be01a3e2a240642619e12f8c95
Megan Rakow <meganrakow@Megans-MBP-2.lan>
2022-10-05 16:21:09 -0500

    edits to JS

--- script.js
+++ script.js
@@ -7,3 +7,27 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+var inputEl= document.querySelector('#location-input');
+var formEl =document.querySelector('form');
+var apiKey = '36bec115901ddc1cb3dd725c6d1f7f33';
+
+function searchHandler (event){
+    event.preventDefault();
+
+    var location = inputEl;
+    
+    var cityUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey;
+
+    fetch(cityUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(city){
+        console.log(city)
+    })
+}
+
+
+
+
+formEl.addEventListener('submit', searchHandler )
